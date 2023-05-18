d90c2b8a25dbb3fc4274aa4e549b29c270455c80
antonio di nocera <antonio.din74@gmail.com>
2023-05-18 11:44:22 +0200

    Prima release

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+import Navbar from './components/navbar';
+import Card from './components/card';
+
+import california from './images/california.png'
+import dragon from './images/dragon.png'
+import dynamite from './images/dynamite.png'
+import philadelphia from './images/philadelphia.png'
+import rainbow from './images/rainbow.png'
+import shrimp from './images/shrimp.png'
+
+class App extends Component {
+  state = {
+    cards : [
+      { id: 0, nome: "California", prezzo:  1.99, immagine: california, quantita: 0},
+      { id: 1, nome: "Dragon", prezzo:  4.4, immagine: dragon, quantita: 0},
+      { id: 2, nome: "Philadelphia", prezzo:  4.1, immagine: philadelphia, quantita: 0},
+      { id: 3, nome: "Rainbow", prezzo:  5.214, immagine: rainbow, quantita: 0},
+      { id: 4, nome: "Dynamite", prezzo:  2.4, immagine: dynamite, quantita: 0},
+      { id: 5, nome: "Shrimp", prezzo:  0.34, immagine: shrimp, quantita: 0},
+    ]
+  }
+
+  handleDelete = cardId => {
+    const cards = this.state.cards.filter(card => card.id !== cardId);
+    this.setState({ cards });
+  }
+  handleIncrement = card => {
+    const cards = [...this.state.cards];
+    const id = cards.indexOf(card);
+    cards[id] = { ...card };
+    cards[id].quantita++;
+    this.setState({ cards });
+  }
+
+
+  render () {
+    return (
+      <>
+        <Navbar />
+        <div className='container'> 
+          <h1>Cosa desideri ordinare?</h1>
+          <hr />
+          <div className='row'>
+          {this.state.cards.map(card => (
+            <Card
+            key={card.id}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+            card={card}
+            />
+          ))}
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
